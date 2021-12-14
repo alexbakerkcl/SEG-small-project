8612f68b7a7800df90f57486348fd900f622f766
Alexander Baker <alexander.m.baker@kcl.ac.uk>
2021-12-14 20:39:01 +0000

    Seeder code added (no unseeder yet)

--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -1,7 +1,27 @@
 from django.core.management.base import BaseCommand, CommandError
+from faker import Faker
+from faker.providers import DynamicProvider
 
 class Command(BaseCommand):
     """The database seeder."""
+    def __init__(self):
+        super().__init__()
+        self.faker = Faker('en_GB')
+
+        experience_provider = DynamicProvider(
+            provider_name="experience",
+            elements = ['A','E','I','B','N'],
+        )
+
+        self.faker.add_provider(experience_provider)
+
 
     def handle(self, *args, **options):
-        print("TODO: The database seeder will be added here...")
+        #print("TODO: The database seeder will be added here...")
+        for i in range(100):
+            user = User.objects.create_user(
+                self.faker.name(),
+                experience=faker.experience(),
+                statement = self.faker.text(),
+                bio = self.faker.text()
+            )
